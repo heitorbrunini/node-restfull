32610a9fd933940dce3c5478b1015ee131cb832f
heitorbrunini <heitorbrunini.ifpb@gmail.com>
2022-03-28 18:36:56 -0300

    GET

--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,47 @@
-let NeDB = require ('nedb');
+let NeDB = require('nedb');
 
 let db = new NeDB(
     {
-        filename:  'users.db',
+        filename: 'users.db',
         autoload: true
     }
 )
 
-module.exports = (app) =>  {
+module.exports = (app) => {
     app.get('/users',
         (req, res) => {
-            res.statusConde = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(
-                {
-                    users: [
-                        {
-                            name: 'test1',
-                            email: 'test1@gmail.com',
-                            password:'123',
-                            id: 1
-                        },
-                        {
-                            name: 'test2',
-                            email: 'test2@gmail.com',
-                            password:'123',
-                            id: 2
-                        }
-                    ]
-                }
-            );
-        }
-    );
-    app.post('/users',  (req, res) => {
-            /*params: object to save, (function if errors, register of DB)  */
-            db.insert(req.body, (err,user) => {
-                if (err){
+
+            db.find({}).sort({ name: 1 }).exec((err, users) => {
+                if (err) {
                     console.log(`error: ${err}`);
                     res.status(400).json({
-                        error:err
+                        error: err
                     })
                 } else {
-                    res.status(200).json(user);
+                    res.statusConde = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(
+                        {
+                            users: users
+                        }
+                    );
                 }
             });
-        }    
+
+        }
+    );
+    app.post('/users', (req, res) => {
+        /*params: object to save, (function if errors, register of DB)  */
+        db.insert(req.body, (err, user) => {
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+                res.status(200).json(user);
+            }
+        });
+    }
     )
 };
